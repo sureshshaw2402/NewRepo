35153fa2fc7c5feb381d89970a334f501ddd2710
sureshshaw2402 <sureshshaw2402@gmail.com>
2017-12-21 19:30:06 +0530

    Code changes by sureshshaw2402

--- src/com/lexmark/selenium/automation/TestDemo.java
+++ src/com/lexmark/selenium/automation/TestDemo.java
@@ -3,7 +3,7 @@ package com.lexmark.selenium.automation;
 public class TestDemo {
 
 	public static void main(String[] args) {
-		System.out.println("sureshshawMac");
+		System.out.println("sureshshaw2402");
 	}
 
 }
