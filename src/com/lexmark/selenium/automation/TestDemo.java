ce77503794c4034bea73391e8e38b590895c4e4c
sureshshaw2402 <sureshshaw2402@gmail.com>
2017-12-22 15:58:15 +0530

    win10 latest changes

--- src/com/lexmark/selenium/automation/TestDemo.java
+++ src/com/lexmark/selenium/automation/TestDemo.java
@@ -3,7 +3,7 @@ package com.lexmark.selenium.automation;
 public class TestDemo {
 
 	public static void main(String[] args) {
-		System.out.println("sureshshawMac New Changes");
+		System.out.println("sureshshawMac New C");
 		System.out.println("sureshshaw2402WIN10");
 	}
 
