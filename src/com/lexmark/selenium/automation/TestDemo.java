a4c3148d22d8f8a9a2be50c0b1b0ad1cb78527ab
sureshshaw <sureshcsegcect@gmail.com>
2017-12-22 15:36:44 +0530

    Merge branch 'master' of https://github.com/sureshshaw2402/NewRepo

--- src/com/lexmark/selenium/automation/TestDemo.java
+++ src/com/lexmark/selenium/automation/TestDemo.java
@@ -3,8 +3,8 @@ package com.lexmark.selenium.automation;
 public class TestDemo {
 
 	public static void main(String[] args) {
-		System.out.println("sureshshawMac New Changes");
-		System.out.println("sureshshaw2402WIN10");
+		System.out.println("sureshshaw Mac New Changes");
+		System.out.println("sureshshaw WIN10");
 	}
 
 }
